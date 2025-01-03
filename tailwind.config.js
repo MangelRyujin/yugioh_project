3805bad740d45c0e60f2086a105cd4494b44b283
mangelryujin <mangelryujin@gmail.com>
2025-01-02 20:47:24 -0500

    feat[landing]: create section about us and factions

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,32 @@ module.exports = {
     flyonui: {
       themes: ["light", "dark", "gourmet"]
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+
+      },
+      'animation': {
+            'text':'text 5s ease infinite',
+        },
+        'keyframes': {
+            'text': {
+                '0%, 100%': {
+                   'background-size':'200% 200%',
+                    'background-position': 'left center'
+                },
+                '50%': {
+                   'background-size':'200% 200%',
+                    'background-position': 'right center'
+                }
+            },
+        }
+    },
   },
   plugins: [
     require("flyonui"),
     require("flyonui/plugin"),
     require("tailwindcss-motion"), // Require only if you want to use FlyonUI JS component
   ],
+  
 }
 
