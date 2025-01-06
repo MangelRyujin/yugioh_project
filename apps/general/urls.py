fe4f397c03b19c4212453b244623ea01e80982e3
mangelryujin <mangelryujin@gmail.com>
2025-01-05 20:08:49 -0500

    feat[dashboard]: Create index for dashboard view

--- apps/general/urls.py
+++ apps/general/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+from apps.general.views.dashboard_view import dashboard
 from .views.home import home
 
 
 urlpatterns = [
     path('', home, name='index'),   
+    path('administración/', dashboard, name='dashboard'),   
 ]
