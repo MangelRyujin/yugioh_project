278f5a698128091603b388230cf07b03f7130f1e
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-01-08 15:57:20 -0500

    feat[cards/search]: filter album

--- apps/card/urls/album_urls.py
+++ apps/card/urls/album_urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from apps.card.views.album_view import album, search_cards, add_card
+from apps.card.views.album_view import album, add_card, search_cards_modal, search_user_cards
 
 
 
 urlpatterns = [
     path('', album, name='album'),   
-    path('search/', search_cards, name='search_cards'),
+    path('search/', search_cards_modal, name='search_cards_modal'),
+    path('search-user-cards/', search_user_cards, name='search_user_cards'),
     path('add-card/', add_card, name='add_card'),
 ]
