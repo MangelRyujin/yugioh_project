12cd77557156fd3f16c3df9f74383076719d9d29
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-01-07 23:11:55 -0500

    feat[models]: adding new fields to models

--- apps/card/models.py
+++ apps/card/models.py
@@ -4,29 +4,35 @@ from apps.accounts.models import User
 
 # Create your models here.
 
-
-
 class Album(models.Model):
+    
+    name = models.CharField(max_length=100, blank=False, null=False, default='Album')
+    cardsQuantity = models.PositiveIntegerField(default=0, blank=False, null=False)
+    total_price = models.FloatField(default=0.00, blank=True, null=False)
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-
+    
     class Meta:
         verbose_name = "Album"
         verbose_name_plural = "Albumes"
 
     def __str__(self):
-        return self.pk
+        return str(self.name)
 
 
 class AlbumCard(models.Model):
     
+    price = models.FloatField(blank=False, null=False, default=0.00)
+    stock = models.PositiveBigIntegerField(default=1, blank=False, null=False)
+    konami_id = models.CharField(max_length=8, blank=False, null=False, default='00000000')
+    real_image = models.ImageField(upload_to='example/', blank=True, null=True)
+    album = models.ForeignKey(Album, related_name='cards', on_delete=models.CASCADE)
     
-
     class Meta:
         verbose_name = "AlbumCard"
         verbose_name_plural = "AlbumCards"
 
     def __str__(self):
-        return self.pk
+        return str(self.konami_id)
 
     
 
