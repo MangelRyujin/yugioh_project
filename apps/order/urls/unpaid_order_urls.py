f8e5fe9c46cd3f34cd8da42358eceaaf6b7577e3
mangelryujin <mangelryujin@gmail.com>
2025-01-15 14:58:55 -0500

    feat[order]: Create UX/UI for paid and unpaid orders

--- apps/order/urls/unpaid_order_urls.py
+++ apps/order/urls/unpaid_order_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from apps.order.views.order_view import order
 from apps.order.views.unpaid_order_views import unpaid_order
 
 urlpatterns = [
-    path('', order, name='order'), 
+    path('', unpaid_order, name='unpaid_order'),    
+       
 ]
