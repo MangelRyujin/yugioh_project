8a03e4ad2ba5dfa11fd701376b58a0a501657239
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-01-26 23:28:32 -0500

    ref[shop/filters]: refactoring filters code

--- apps/card/filters.py
+++ apps/card/filters.py
@@ -14,21 +14,31 @@ class AlbumCardFilter(django_filters.FilterSet):
     attribute = django_filters.CharFilter(method='filter_attribute')
     archetype = django_filters.CharFilter(lookup_expr='icontains')
 
-    RARITY_MAP = dict(AlbumCard.RARITY_CARD)
-    VERSION_MAP = dict(AlbumCard.VERSION_CARD)
-    ATTRIBUTE_MAP = dict(AlbumCard.ATTRIBUTE_CARD)
+    #RARITY_MAP = dict(AlbumCard.RARITY_CARD)
+    RARITY_MAP = {key: value for key, value in AlbumCard.RARITY_CARD}
+    VERSION_MAP = {key: value for key, value in AlbumCard.VERSION_CARD}
+    ATTRIBUTE_MAP = {key: value for key, value in AlbumCard.ATTRIBUTE_CARD}
 
     def filter_rarity(self, queryset, name, value):
-        mapped_value = self.RARITY_MAP.get(value, value)
-        return queryset.filter(rarity=mapped_value)
+        mapped_value = self.RARITY_MAP.get(value, None)
+        if mapped_value:
+            return queryset.filter(rarity=value)
+        else:
+            return queryset
 
     def filter_version(self, queryset, name, value):
-        mapped_value = self.VERSION_MAP.get(value, value)
-        return queryset.filter(version=mapped_value)
+        mapped_value = self.VERSION_MAP.get(value, None)
+        if mapped_value:
+            return queryset.filter(version=value)
+        else:
+            return queryset
 
     def filter_attribute(self, queryset, name, value):
-        mapped_value = self.ATTRIBUTE_MAP.get(value, value)
-        return queryset.filter(attribute=mapped_value)
+        mapped_value = self.ATTRIBUTE_MAP.get(value, None)
+        if mapped_value:
+            return queryset.filter(attribute=value)
+        else:
+            return queryset
 
     def filter_price(self, queryset, name, value):
         exact_match = queryset.filter(**{name: value})
