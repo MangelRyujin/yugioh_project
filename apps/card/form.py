909afa30e99eb76a65ca651739e3b145120d342a
mangelryujin <mangelryujin@gmail.com>
2025-01-31 12:12:35 -0500

    revert[album_card_edit]: revert changes to form for edit album card

--- apps/card/form.py
+++ apps/card/form.py
@@ -1,17 +1,10 @@
 from django import forms
 from apps.card.models import AlbumCard
 
-class CustomDecimalField(forms.DecimalField):
-    def to_python(self, value):
-        return super().to_python(value.replace(',', '.'))
+
         
 class UpdateAlbumCardForm(forms.ModelForm):
-    price = CustomDecimalField(
-        max_digits=10,
-        decimal_places=2,
-        required=True,
-        min_value=0.01
-    )
+    
     class Meta:
         model = AlbumCard
         fields = ["price","stock","rarity","version","expantion"]
