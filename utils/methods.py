ad7f4c46cd8d90af550b0f59da96e2a6b70ce93c
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-02-04 23:04:19 -0500

    ref[filters/shop]: fixing magic/trap filter for type of card

--- utils/methods.py
+++ utils/methods.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 
 def _get_paginator(request, object):
-    paginator = Paginator(object, 6)
+    paginator = Paginator(object, 100)
     page_number = request.GET.get('page', 1)
     page_object = paginator.get_page(page_number)
     
