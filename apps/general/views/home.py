e60d6dfd94ef4ecd2eab166af28c7d323c1826be
mangelryujin <mangelryujin@gmail.com>
2025-03-07 21:51:24 -0500

    feat[logo]:Create logo by project

--- apps/general/views/home.py
+++ apps/general/views/home.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from apps.card.models import AlbumCard,AlbumDecks
+
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    context={
+        'tota_cards': AlbumCard.objects.all().count(),
+        'tota_decks': AlbumDecks.objects.all().count()
+    }
+    return render(request,'index.html',context)
